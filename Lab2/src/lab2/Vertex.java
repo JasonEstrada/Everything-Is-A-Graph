dd2fdc10393a2c6f1e7636492fb086b91941f217
JasonEstrada <98917369+JasonEstrada@users.noreply.github.com>
2022-10-12 21:38:18 -0500

    Add files via upload

---
+++ Lab2/src/lab2/Vertex.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab2;
+
+import java.awt.Font;
+import java.awt.Graphics;
+import javafx.scene.paint.Color;
+
+/**
+ *
+ * @author F-E-R
+ */
+public class Vertex {
+
+    private int x, y;
+    private String name;
+    public static final int radio = 20;
+
+    public Vertex(int x, int y, String name) {
+        this.x = x;
+        this.y = y;
+        this.name = name;
+    }
+
+    public void pintar(Graphics g) {
+        g.setColor(new java.awt.Color(162, 231, 225));
+        g.fillOval(this.x - radio, this.y - radio, radio * 2, radio * 2);
+        g.setColor(java.awt.Color.black);
+        Font myFont = new Font ("Berlin Sans FB", Font.PLAIN, 16);
+        g.setFont(myFont);
+        g.drawString(name, x-radio-5, y-radio-5);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
